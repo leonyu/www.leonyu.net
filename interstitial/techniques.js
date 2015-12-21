1305b1eb561449211f635edce07a7c0802c0ad17
Leon Yu <leon@leonyu.net>
2015-12-21 17:13:13 -0500

    Update techniques.js

--- interstitial/techniques.js
+++ interstitial/techniques.js
@@ -149,7 +149,7 @@ var TECHNIQUES = (function(){
         }
       });
       origin = win.location.href;
-      popup = win.open('javascript:location="'+url+'";setInterval(function(){location="data:text/html,<script>if(opener){opener.postMessage(\'msg\',\''+ origin +'\');setTimeout(function(){opener.postMessage(\'not_installed\',\''+origin+'\');close()},100)}</script>"},50)');
+      popup = win.open('javascript:location="'+url+'";last=Date.now();setInterval(function(){now=Date.now();if(now-last>100){last=now;return};location="data:text/html,<script>if(opener){opener.postMessage(\'msg\',\''+ origin +'\');setTimeout(function(){opener.postMessage(\'not_installed\',\''+origin+'\');close()},100)}</script>"},50)');
     },
   }, {
     name: 'win.open + open + open',
