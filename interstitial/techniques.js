a554ddd02fc465b1b2965277d52ed4c4eee46254
Leon Yu <leon@leonyu.net>
2015-12-21 01:32:32 -0500

    reduce flicker

--- interstitial/techniques.js
+++ interstitial/techniques.js
@@ -77,7 +77,7 @@ var TECHNIQUES = (function(){
               Log.logTo(popup.window.document.body);
               popup.window.location = 'itms-appss://itunes.apple.com/us/app/bitly/id525106063';
             }
-          }, 100);
+          }, 50);
         }
       }
   }, {
@@ -91,7 +91,7 @@ var TECHNIQUES = (function(){
             Log.logTo(popup.window.document.body);
             popup.window.location = 'about:blank';
           }
-        }, 100);
+        }, 50);
       }
     }
   }, {
@@ -105,7 +105,7 @@ var TECHNIQUES = (function(){
             Log.logTo(popup.window.document.body);
             popup.window.location = 'https://bitly.com';
           }
-        }, 100);
+        }, 50);
       }
     }
 }, {
