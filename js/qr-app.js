04af33fef7b7adb495e28fedab13912398748468
Leon Yu <leon@leonyu.net>
2018-07-13 02:32:48 -0400

    remove button

--- js/qr-app.js
+++ js/qr-app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var timeoutId = 0;
   var qrText = document.getElementById('qr-text');
   var qrCode = new QRious({
     element: document.getElementById('qr-code'),
@@ -11,23 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
     qrCode.value = qrText.value;
   });
 
-  var ipButton = document.getElementById('qr-ip-btn');
-  ipButton.addEventListener('click', function () {
-    fetch('https://boxing.leonyu.net/ip.json?=' + Date.now()).then(function(r) {
-      if (r.ok) {
-        return r.json();
-      } else {
-        throw ("" + r.status + " " + r.statusText);
-      }
-    })
-      .then(function(json) {
-        qrText.value = 'CLIENT_IP:' + json.ip;
-        qrCode.value = qrText.value;
-       })
-      .catch(function(err) {
-        console.error(err);
-        qrText.value = err.message;
-        qrCode.value = qrText.value;
+  fetch('https://boxing.leonyu.net/ip.json?=' + Date.now()).then(function(r) {
+    if (r.ok) {
+      return r.json();
+    } else {
+      throw ("" + r.status + " " + r.statusText);
+    }
+  })
+    .then(function(json) {
+      qrText.value = 'CLIENT_IP:' + json.ip;
+      qrCode.value = qrText.value;
       })
-  });
+    .catch(function(err) {
+      console.error(err);
+      qrText.value = err.message;
+      qrCode.value = qrText.value;
+    })
 });
