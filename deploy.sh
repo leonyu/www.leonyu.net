9b643f5816125f5198c377966811c5e125e0bd78
Leon Yu <leon@leonyu.net>
2020-03-28 22:33:26 -0700

    Switch from AWS to GCP

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-yarn
+yarn install
 yarn clean
 yarn lint
 yarn build
-aws s3 cp --acl public-read --recursive ./dist s3://www.leonyu.net/
+gsutil cp -R ./dist gs://www.leonyu.net/
+# aws s3 cp --acl public-read --recursive ./dist s3://www.leonyu.net/
