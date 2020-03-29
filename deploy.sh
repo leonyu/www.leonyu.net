c9502d60d1adde2779c9f1bee1dda7e418e40be9
Leon Yu <leon@leonyu.net>
2020-03-28 22:37:09 -0700

    Expand GCP command

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,10 @@ yarn install
 yarn clean
 yarn lint
 yarn build
-gsutil cp -R ./dist gs://www.leonyu.net/
+
+# GCP
+# gsutil web set -m index.html -e error.html gs://www.leonyu.net/
+gsutil -m cp -R ./dist gs://www.leonyu.net/
+
+# AWS
 # aws s3 cp --acl public-read --recursive ./dist s3://www.leonyu.net/
