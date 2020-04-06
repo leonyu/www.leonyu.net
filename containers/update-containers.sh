2b5e1ff3231aad08c70606f711e500c038638269
Leon Yu <leon@leonyu.net>
2020-04-06 01:52:07 -0700

    Switch docker-compose from AWS to GCP

--- containers/update-containers.sh
+++ containers/update-containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pushd $(dirname "$(readlink -f "$0")")
+set -e
+
+pushd "$(dirname "$(readlink -f "$0")")"
 docker-compose -f ./docker-compose.yml pull
 docker-compose -f ./docker-compose.yml up -d --remove-orphans
 popd
