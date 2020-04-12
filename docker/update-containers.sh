c93e457a82383368d1023b6766c281a43c3a474b
Leon Yu <leon@leonyu.net>
2020-04-12 04:05:32 -0700

    Remove docker folder, moved to its own repo

--- docker/update-containers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-pushd "$(dirname "$(readlink -f "$0")")"
-docker-compose -f ./docker-compose.yml pull
-docker-compose -f ./docker-compose.yml up -d --remove-orphans
-popd
-docker system prune -f
