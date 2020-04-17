75f861be8a76da3770a3e6f96755cd3b23932970
Leon Yu <leon@leonyu.net>
2020-04-16 22:05:35 -0700

    get Check-IP header

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import QRView from './QRView';
 
 declare const fetchCheckIP: Promise<string>;
 
-function init(ipPromise: Promise<string>) {
+async function init(ipPromise: Promise<string>) {
   const view = new QRView();
-  ipPromise.then((ipAddress) => {
-    view.updateInput(`CLIENT_IP:${ipAddress}`);
-  }).catch((err) => {
+  try {
+    const ipAddress = await ipPromise;
+    if (ipAddress) {
+      view.updateInput(`CLIENT_IP:${ipAddress}`);
+    }
+  } catch (err) {
     view.updateInput(`${err}`);
-  });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => init(fetchCheckIP));
