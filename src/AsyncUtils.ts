98b970462adb56eb282b2f0263e88c4afa42aa37
Leon Yu <leon@leonyu.net>
2024-09-20 03:37:00 -0400

    debounce update

--- src/AsyncUtils.ts
+++ src/AsyncUtils.ts
@@ -2,9 +2,12 @@
 type Func<TInput extends unknown[], TOutput> = (...args: TInput) => TOutput;
 
 export function debounce<T extends Func<unknown[], void>>(func: T, wait: number): Func<Parameters<T>, void> {
-  let timeoutId: ReturnType<typeof setTimeout>;
+  let lastRun = 0;
   return (...args: Parameters<T>): void => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => { func(...args); }, wait);
+    const now = Date.now();
+    if (now > lastRun + wait) {
+      lastRun = now;
+      func(...args);
+    }
   };
 }
